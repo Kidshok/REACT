849442383a57d455618864bcc803611842e4cb2a
Kidshok <Kidshok53@gmail.com>
2021-11-29 19:42:30 +0300

    lesson-1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Message from './App';
 import reportWebVitals from './reportWebVitals';
 
+const myName = 'Danil'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Message name={myName} />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
